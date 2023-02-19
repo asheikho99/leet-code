ce131f8327313a17e1da52e6d4c0a61b7486b92f
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-02-19 11:43:22 -0400

    [2129] Capitalize the Title
    200/200 cases passed (57 ms)
    Your runtime beats 95.11 % of javascript submissions
    Your memory usage beats 40.49 % of javascript submissions (42.6 MB)

---
+++ 2129-capitalize-the-title/README.md
@@ -0,0 +1,4 @@
+# Results
+* 200/200 cases passed (57 ms)
+* Your runtime beats 95.11 % of javascript submissions
+* Your memory usage beats 40.49 % of javascript submissions (42.6 MB)
--- 2129-capitalize-the-title/solution.js
+++ 2129-capitalize-the-title/solution.js
@@ -10,9 +10,18 @@
  * @return {string}
  */
 var capitalizeTitle = function (title) {
-  return title.split(" ").map((word) => {
-    word = word.toLowerCase()
-    if (word.length >= 1 && word.length <= 2) return word
-  })
+  return [...title.split(" ").map((word) => {
+      word = word.toLowerCase()
+
+      if (word.length >= 1 && word.length <= 2) return word
+
+      let charArr = word.split("")
+      let fChar = charArr.shift().toUpperCase()
+
+      charArr.unshift(fChar)
+
+      return charArr.join("")
+    }),
+  ].join(" ")
 }
 // @lc code=end
